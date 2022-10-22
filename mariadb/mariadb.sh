eadb93ff2652616752af01fdb8f2dbbfb5549f55
Meriem Mounchid <mmounchi@e2r5p13.1337.ma>
2022-10-22 17:51:05 +0100

    mandatory part 90%

--- mariadb/mariadb.sh
+++ mariadb/mariadb.sh
@@ -1,38 +1,17 @@
-#### Creat Debian Container ####
-docker pull debian
-docker run -it -p 3306:3306 --name mymariadb debian
-docker run -it -p 3306:3306 -v /Users/mmounchi/data/ --name myMariaDB maria
+#!/bin/sh
 
-#### Install MariaDB ####
-apt-get update
-apt-get install -y mariadb-server vim 
+service mysql start
+# mysql -u root < mariadb.sql
 
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE" 
+mysql -u root -e "CREATE USER '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ROOT_USER'@'%'"
+mysql -u root -e "USE $MYSQL_DATABASE"
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'"
+mysql -u root -e "FLUSH PRIVILEGES"
 
-# export NAME=VALUE
+echo "Mariadb is Running.."
+service mysql stop
 
-export MYSQL_ROOT_PASSWORD=root_misaki
-export MYSQL_PASSWORD=misaki
-export MYSQL_USER=misaki_user
-export MYSQL_DATABASE=misaki_db
-
-service mariadb start
-# RUN /etc/init.d/mariadb start
-mariadb
-CREATE DATABASE misaki_db;
-
-# show databases;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '';
-CREATE USER 'master'@localhost IDENTIFIED BY 'master';
-CREATE USER 'master'@'%' IDENTIFIED BY 'master';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'master'@localhost;
-CREATE USER 'worker'@localhost IDENTIFIED BY 'worker'; 
-
-# Save Changes
-FLUSH PRIVILEGES;
-# See details: SHOW GRANTS FOR 'master'@localhost;
-# Delet User: DROP USER myuser@localhost;
-# See All Users: select user from mysql.user;
-# Connect: mysql -u NAME -p
-
-# ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('root');
-# docker inspect MyMariaDB | grep -C3 -e "Volumes\":"
+mysqld_safe
--- mariadb/mariadb.sql
+++ mariadb/mariadb.sql
@@ -6,6 +6,16 @@ CREATE USER 'misaki_user'@'%' IDENTIFIED BY 'misaki';
 GRANT ALL PRIVILEGES ON misaki_db.* TO 'misaki_user'@'%';
 FLUSH PRIVILEGES;
 
+-- CREATE DATABASE IF NOT EXISTS '{$MYSQL_DATABASE}';
+-- CREATE USER '{$MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '{$MYSQL_ROOT_PASSWORD}';
+-- GRANT ALL PRIVILEGES ON *.* TO '{$MYSQL_ROOT_USER}'@'%';
+-- USE misaki_db;
+-- CREATE USER '{$MYSQL_USER}'@'%' IDENTIFIED BY '{$MYSQL_PASSWORD}';
+-- GRANT ALL PRIVILEGES ON misaki_db.* TO '{$MYSQL_USER}'@'%';
+-- FLUSH PRIVILEGES;
+
+
+
 
 -- Reload the grant tables into memory without reloading or restarting mysql service.
 -- See details: SHOW GRANTS FOR 'master'@localhost;
