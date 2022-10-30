f7bd2dd3ab18bf5836e1cc3014c51330f6d52bb1
Meriem Mounchid <mmounchi@e2r5p13.1337.ma>
2022-10-30 16:40:20 +0100

    add wp-cli

--- mariadb/mariadb.sh
+++ mariadb/mariadb.sh
@@ -3,15 +3,16 @@
 service mysql start
 # mysql -u root < mariadb.sql
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE" 
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
 mysql -u root -e "CREATE USER '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ROOT_USER'@'%'"
-mysql -u root -e "USE $MYSQL_DATABASE"
+# mysql -u root -e "USE $MYSQL_DATABASE"
 mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'"
+mysql -u root -e "USE $MYSQL_DATABASE"
 mysql -u root -e "FLUSH PRIVILEGES"
 
-echo "Mariadb is Running.."
 service mysql stop
 
+echo "Mariadb is Running.."
 mysqld_safe
