a6b83606c61bb7bdd9da6cca5fff0a45c0eec159
Meriem Mounchid <mmounchi@e2r5p12.1337.ma>
2022-11-16 18:34:28 +0100

    cadvisor added sucsessfullly

--- mariadb/mariadb.sh
+++ mariadb/mariadb.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-service mysql start
-
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
-mysql -u root -e "CREATE USER '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ROOT_USER'@'%'"
-# mysql -u root -e "USE $MYSQL_DATABASE"
 mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'"
-mysql -u root -e "USE $MYSQL_DATABASE"
 mysql -u root -e "FLUSH PRIVILEGES"
-
-service mysql stop
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'"
+mysql -u root -e "UPDATE mysql.user SET host='%' WHERE user='root'"
+mysql -u root -e "FLUSH PRIVILEGES"
+mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
 
 echo "Mariadb is Running.."
-mysqld_safe
--- nginx/script.sh
+++ nginx/script.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-
-# service nginx start
-# spleep 5
 echo "Nginx is Running.."
 nginx -g 'daemon off;'
-# /usr/sbin/nginx
